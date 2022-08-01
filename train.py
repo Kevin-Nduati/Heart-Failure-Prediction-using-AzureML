e3355f0fa53a5a1dc97b2b0f09b89d000e7b8a07
Kevin Nduati <78505752+Kevin-Nduati@users.noreply.github.com>
2022-08-01 18:52:30 +0000

    final files

--- train.py
+++ train.py
@@ -10,17 +10,20 @@ import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
+dataset_path = "https://github.com/Kevin-Nduati/Heart-Failure-Prediction-using-AzureML/blob/71c29d71430511ab531bf353f1c1287f00c022f3/heart_failure_clinical_records_dataset.csv"
+
+ds = TabularDatasetFactory.from_delimited_files(path=dataset_path)
+
+x_df = ds.to_pandas_dataframe().dropna()
+
 run = Run.get_context()
 
-def main():
+y_df = x_df.pop("DEATH_EVENT")
 
-    url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/00519/heart_failure_clinical_records_dataset.csv'
-    data = TabularDatasetFactory.from_delimited_files(url)
-    x = data.to_pandas_dataframe()
-    y = x.pop("DEATH_EVENT")
-    x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=625, shuffle=True)   
-    
-    # Add arguments to script+-
+x_train, x_test, y_train, y_test = train_test_split(x_df, y_df, test_size=0.3, random_state=123)
+
+def main():
+    # Add arguments to script
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
@@ -34,10 +37,12 @@ def main():
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
     accuracy = model.score(x_test, y_test)
+    
+    os.makedirs('outputs-hdr',exist_ok = True)
+    
+    joblib.dump(model,'outputs-hdr/model.joblib')
+    
     run.log("Accuracy", np.float(accuracy))
-   
-    os.makedirs('./outputs', exist_ok=True)
-    joblib.dump(value=model,filename='./outputs/model.joblib')
 
 if __name__ == '__main__':
     main()
